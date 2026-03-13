8c15a3c5db9891534f1a2855d2de1269a5701c4d
Stan Carver II <stan@a1webconsulting.com>
2026-03-13 12:21:00 -0500

    restored prompts

--- bin/_lib.sh
+++ bin/_lib.sh
@@ -22,6 +22,41 @@ banner() {
 }
 
 ########################################
+# Prompts
+########################################
+
+prompt() {
+  read -r -p "$1 " REPLY
+  [[ "$REPLY" == "y" ]]
+}
+
+select_menu() {
+  local prompt="$1"
+  shift
+  local options=("$@")
+
+  echo ""
+  echo "$prompt"
+  echo "================"
+
+  local i=1
+  for opt in "${options[@]}"; do
+    echo "$i) $opt"
+    ((i++))
+  done
+
+  echo ""
+
+  read -r -p "Select option: " choice
+
+  if [[ "$choice" =~ ^[0-9]+$ ]] && (( choice >= 1 && choice <= ${#options[@]} )); then
+    echo "${options[$((choice-1))]}"
+  else
+    fail "Invalid option"
+  fi
+}
+
+########################################
 # Messaging helpers
 ########################################
 
