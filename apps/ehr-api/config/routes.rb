539e4d4683ee78968f7950b638d002f8621e1d58
Stan Carver II <stan@a1webconsulting.com>
2026-03-13 16:54:11 -0500

    added api/up route to match portal health route

--- apps/ehr-api/config/routes.rb
+++ apps/ehr-api/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   # get "up" => "rails/health#show", as: :rails_health_check
   get "/up", to: proc { [200, {}, ["ok"]] }
+  get "/api/up", to: proc { [200, {}, ["ok"]] }
 
   post "/graphql", to: "graphql#execute"
   devise_for :users
