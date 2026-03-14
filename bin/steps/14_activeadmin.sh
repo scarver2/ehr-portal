591d0e876fb212bd757ed33bdc2542a04ea0ff2e
Stan Carver II <stan@a1webconsulting.com>
2026-03-14 17:31:23 -0500

    ActiveAdmin (#2)
    * cleansed Dockerfiles
    
    * added ActiveAdmin step to bootstrap
    
    * ignored sass build artifacts
    
    * added ActiveAdmin and asset gems
    
    * switch ApplicationController from API to Base
    
    * added Foreman to run DX processes
    
    * added Rails middleware for ActiveAdmin
    
    * added ActiveAdmin routes
    
    * added AdminUsers schema
    
    * Added AdminUser seed for development
    
    * added default Arbre views for ActiveAdmin dashboards
    
    * added ActiveAdmin assets
    
    * added AdminUser model, schema, and specs
    
    * added ActiveAdmin initializer
    
    * added ActiveAdmin assets initializer
    
    * added DART SASS initializer
    
    * added ActiveAdminComments polymorphic schema
    
    * added ActiveAdmin configuration and assets to bootstrap
    
    * added database config
    
    * link ActiveAdmin title to /admin
    
    * added Postgres accessory to API server
    
    * added bin/kamal script for server maangement
    
    * corrected Postgres accessory and secrets
    
    * compile ActiveAdmin's assets

---
+++ bin/steps/14_activeadmin.sh
@@ -0,0 +1,124 @@
+#!/usr/bin/env bash
+# bin/steps/13_active-admin.sh
+
+source "$(dirname "$0")/../_lib.sh"
+
+cd apps/ehr-api
+
+info "Adding ActiveAdmin..."
+# Should already be installed bundle add devise
+bundle add activeadmin
+
+
+info "Configuring Rails to serve ActiveAdmin..."
+# In apps/ehr-api/app/controllers/application_controller.rb
+# replace class ApplicationController < ActionController::API
+# with class ApplicationController < ActionController::Base
+
+
+
+info "Adding asset pipeline..."
+bundle add propshaft
+bundle add dartsass-rails
+# TODO: Append to Procfile.dev
+# apps/ehr-api/Procfile.dev
+# css: bin/rails dartsass:watch
+
+# TODO: In apps/ehr-api/config/application.rb
+# replace config.api_only = true with config.api_only = false
+# require "action_view/railtie"
+# insert:
+# config.middleware.use ActionDispatch::Cookies
+# config.middleware.use ActionDispatch::Session::CookieStore
+# config.middleware.use ActionDispatch::Flash
+
+
+cat <<EOF > config/initializers/assets.rb
+# apps/ehr-api/config/initializers/assets.rb
+Rails.application.config.assets.precompile += %w(
+  active_admin.js
+  active_admin.css
+)
+EOF
+
+# config/initializers/session_store.rb
+# Rails.application.config.session_store :cookie_store, key: "_ehr_admin_session"
+
+# config.action_dispatch.cookies_same_site_protection = :lax
+
+# In config/initializers/devise.rb
+# find and replace config.navigational_formats = ['*/*', :html, :turbo_stream]
+# with config.navigational_formats = ['*/*', :html]
+
+# In config/initializers/devise.rb
+# find and replace config.site_title = "Ehr Api"
+# with config.site_title = "EHR Admin"
+
+
+# config/application.rb
+bin/rails generate active_admin:install
+
+# TODO: In apps/ehr-api/config/initializers/active_admin.rb
+# replace config.site_title = "Ehr Api"
+# with config.site_title = "EHR Portal Admin"
+
+# mkdir -p app/assets/stylesheets
+# mkdir -p app/assets/javascripts
+
+# touch app/assets/stylesheets/active_admin.css
+# touch app/assets/javascripts/active_admin.js
+
+# config/application.rb
+# require "action_view/railtie"
+# config.middleware.use ActionDispatch::Cookies
+# config.middleware.use ActionDispatch::Session::CookieStore
+# config.middleware.use ActionDispatch::Flash
+
+# app/assets/stylesheets/active_admin.css
+# /*
+#  *= require active_admin/base
+#  */
+
+# app/assets/javascripts/active_admin.js
+# //
+# //= require active_admin/base
+# //
+
+
+cat <<EOF >> config/initializers/assets.rb
+# apps/ehr-api/config/initializers/assets.rb
+# frozen_string_literal: true
+Rails.application.config.assets.precompile += %w(
+  active_admin.js
+  active_admin.css
+)
+EOF
+
+# config/initializers/dartsass.rb
+# frozen_string_literal: true
+
+# TODO: DART SASS initializer
+# Rails.application.config.dartsass.builds = {
+#   "application.scss"    => "application.css",
+#   "active_admin.scss"   => "active_admin.css"
+# }
+
+# app/views/layouts/active_admin.html.erb
+# <%= stylesheet_link_tag "active_admin", "data-turbo-track": "reload" %>
+# <%= javascript_include_tag "active_admin", "data-turbo-track": "reload" %>
+
+info "Adding Administrator user..."
+# bin/rails generate devise:install
+# bin/rails generate devise AdminUser
+
+# In app/models/admin_user.rb
+# insert after class AdminUser < ApplicationRecord
+#   def self.ransackable_attributes(auth_object = nil)
+#     ["created_at", "email", "encrypted_password", "id", "id_value", "remember_created_at", "reset_password_sent_at", "reset_password_token", "updated_at"]
+#   end
+
+
+# info "Migrating database..."
+# bin/rails db:migrate
+
+# TODO config ActiveAdmin to use Devise for authentication
