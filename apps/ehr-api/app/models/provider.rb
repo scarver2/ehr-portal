2c719f8dc84a25193d10e07cf022fe014598b949
Stan Carver II <stan@a1webconsulting.com>
2026-03-15 23:26:22 -0500

    added Ransack to Provider model

--- apps/ehr-api/app/models/provider.rb
+++ apps/ehr-api/app/models/provider.rb
@@ -1,2 +1,7 @@
+# apps/ehr-api/app/models/provider.rb
+
 class Provider < ApplicationRecord
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "first_name", "id", "id_value", "last_name", "updated_at"]
+  end
 end
