6fcd335c0f741e6144b7dded11f26f17ae81d592
Stan Carver II <stan@a1webconsulting.com>
2026-03-15 22:48:52 -0500

    added additional health check endpoints to API

--- apps/ehr-api/config/routes.rb
+++ apps/ehr-api/config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  # get "/up", to: proc { [200, {}, ["ok"]] }
+  get "/healthz", to: proc { [200, {}, ["ok"]] }
+  get "/up", to: proc { [200, {}, ["ok"]] }
   get "/api/up", to: proc { [200, {}, ["ok"]] }
 
   post "/graphql", to: "graphql#execute"
