3144d3458a0e538e8fcdd5097b65e1af88ed20e5
Stan Carver II <stan@a1webconsulting.com>
2026-03-17 08:32:06 -0500

    added descriptions to GraphQL nodes

--- apps/ehr-api/app/graphql/types/node_type.rb
+++ apps/ehr-api/app/graphql/types/node_type.rb
@@ -3,6 +3,8 @@
 module Types
   module NodeType
     include Types::BaseInterface
+
+    description 'An object with a globally unique ID.'
     # Add the `id` field
     include GraphQL::Types::Relay::NodeBehaviors
   end
